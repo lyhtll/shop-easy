b0114c8f023ec06d1875badcc47c672f9a9f0f06
lyhtll <a62346986@gmail.com>
2026-05-20 09:33:04 +0900

    chore: @ConfigurationPropertiesScan 추가 및 application.properties 제거

--- src/main/java/com/yunha/shopeasy/ShopEasyApplication.java
+++ src/main/java/com/yunha/shopeasy/ShopEasyApplication.java
@@ -2,12 +2,13 @@ package com.yunha.shopeasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ShopEasyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopEasyApplication.class, args);
     }
-
 }
